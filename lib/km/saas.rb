0338c9ceb38f3b543e3f1b5f9948243bcc210d6f
Daniel Bretoi <daniel@netwalk.org>
2011-02-07 10:30:12 -0800

    ignore '' as well as nil

--- lib/km/saas.rb
+++ lib/km/saas.rb
@@ -2,24 +2,24 @@ require 'km'
 class KM
   module SaaS
     def signed_up(plan=nil, props = {})
-      props['Plan Name'] = plan if plan
+      props['Plan Name'] = plan unless plan.to_s.empty?
       record 'Signed Up', props
     end
     alias signedup signed_up
 
     def upgraded(plan=nil, props = {})
-      props['Plan Name'] = plan if plan
+      props['Plan Name'] = plan unless plan.to_s.empty?
       record 'Upgraded', props
     end
 
     def downgraded(plan=nil, props = {})
-      props['Plan Name'] = plan if plan
+      props['Plan Name'] = plan unless plan.to_s.empty?
       record 'Downgraded', props
     end
 
     def billed(amount=nil, description=nil, props={})
-      props['Billing Amount']      = amount if amount
-      props['Billing Description'] = description if description
+      props['Billing Amount']      = amount unless amount.to_s.empty?
+      props['Billing Description'] = description unless description.to_s.empty?
       record 'Billed', props
     end
 
@@ -29,8 +29,8 @@ class KM
     alias cancelled canceled
 
     def visited_site(url=nil, referrer=nil, props={})
-      props['URL']      = url if url
-      props['Referrer'] = referrer if referrer
+      props['URL']      = url unless url.to_s.empty?
+      props['Referrer'] = referrer unless referrer.to_s.empty?
       record 'Visited Site', props
     end
     # ------------------------------------------------------------------------
