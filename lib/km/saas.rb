b78ef33e2b3c514371108a831766f2f5a02446b9
Daniel Bretoi <daniel@netwalk.org>
2011-02-07 09:53:11 -0800

    change hashrockets

--- lib/km/saas.rb
+++ lib/km/saas.rb
@@ -2,24 +2,24 @@ require 'km'
 class KM
   module SaaS
     def signed_up(plan=nil, props = {})
-      props['Plan Name'] => plan if plan
+      props['Plan Name'] = plan if plan
       record 'Signed Up', props
     end
     alias signedup signed_up
 
     def upgraded(plan=nil, props = {})
-      props['Plan Name'] => plan if plan
+      props['Plan Name'] = plan if plan
       record 'Upgraded', props
     end
 
     def downgraded(plan=nil, props = {})
-      props['Plan Name'] => plan if plan
+      props['Plan Name'] = plan if plan
       record 'Downgraded', props
     end
 
     def billed(amount=nil, description=nil, props={})
-      props['Billing Amount'] => amount if amount
-      props['Billing Description'] => description if description
+      props['Billing Amount']      = amount if amount
+      props['Billing Description'] = description if description
       record 'Billed', props
     end
 
@@ -29,8 +29,8 @@ class KM
     alias cancelled canceled
 
     def visited_site(url=nil, referrer=nil, props={})
-      props['URL'] => url if url
-      props['Referrer'] => referrer if referrer
+      props['URL']      = url if url
+      props['Referrer'] = referrer if referrer
       record 'Visited Site', props
     end
     # ------------------------------------------------------------------------
